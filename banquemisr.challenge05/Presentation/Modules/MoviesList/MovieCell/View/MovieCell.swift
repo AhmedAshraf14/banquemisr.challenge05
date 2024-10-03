cc7beabc1d32b2795483fe2bce6f2f66968ef320
AhmedAshraf14 <a.ashraf.dev@gmail.com>
2024-10-04 02:03:48 +0300

    design the three tabs on the storyboard

---
+++ banquemisr.challenge05/Presentation/Modules/MoviesList/MovieCell/View/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  banquemisr.challenge05
+//
+//  Created by Ahmed Ashraf on 03/10/2024.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ banquemisr.challenge05/Presentation/Modules/MoviesList/ViewModel/MovieListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieListViewModel.swift
+//  banquemisr.challenge05
+//
+//  Created by Ahmed Ashraf on 03/10/2024.
+//
+
+import Foundation
